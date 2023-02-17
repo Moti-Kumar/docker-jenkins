77665c12ac80da4fd1daffab91891dfcfee747a4
Moti <moti.kumar1110@gmail.com>
2023-02-17 18:40:54 +0530

    DockerSampleFirstCommit

---
+++ src/main/java/com/example/docker/CurdController.java
@@ -0,0 +1,15 @@
+package com.example.docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurdController {
+	
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Welcome to Docker Tutorial...";
+		
+	}
+
+}
